fd83d4ad4c446f3b1ca21ded277930140427f305
ismail safha <ismailsafha@gmail.com>
2023-03-17 11:35:40 +0100

    fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Header from "../components/Header";
 
 interface HomeProps {
   posts: any[];
@@ -15,7 +16,7 @@ export default function Home({ posts }: HomeProps) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <p>hi</p>
+      <Header />
     </>
   );
 }
